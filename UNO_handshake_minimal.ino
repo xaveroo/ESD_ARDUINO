abf98682543fbe828bd0620633c6fd279b52b2c8
xaveroo <64372866+xaveroo@users.noreply.github.com>
2022-11-09 16:58:15 +0100

    Update UNO_handshake_minimal.ino

--- UNO_handshake_minimal.ino
+++ UNO_handshake_minimal.ino
@@ -1,5 +1,9 @@
 #include <SPI.h>
 
+#define handshake 1
+#define send_val 69
+
+
 // #define DATAOUT     11  //COPI
 // #define DATAIN      12  //CIPO
 // #define SPICLOCK    13  //sck
@@ -11,23 +15,21 @@ char buff [50];
 byte c;
 volatile byte indx;
 volatile boolean received;
-volatile boolean answer;
+
 
 void setup (void) {
    Serial.begin (115200);
    while(!Serial){;}
    Serial.println("Serial initiated");
-   pinMode(MISO, OUTPUT);           // have to send on master in so it set as output
-   SPCR |= _BV(SPE);                // turn on SPI in slave mode
-   indx = 0;                        // buffer empty
-   SPI.attachInterrupt();           // turn on interrupt
+   pinMode(MISO, OUTPUT);               // have to send on master in so it set as output
+   SPCR |= _BV(SPE);                    // turn on SPI in slave mode
+   indx = 0;                            // buffer empty
+   SPI.attachInterrupt();               // turn on interrupt
 }
-ISR(SPI_STC_vect){                  // SPI interrupt routine { 
-   c = SPDR;                        // read byte from SPI Data Register
-   received=true;                   // raise flags
-   answer=true;
-   
-  //  
+ISR(SPI_STC_vect){                      // SPI interrupt routine { 
+   c = SPDR;                            // read byte from SPI Data Register
+   received=true; 
+  // buffer longer messages (inside service routine??)
   //  if (indx < sizeof buff) {
   //     buff[indx++] = c; // save data in the next index in the array buff
   //     // if (c == '\r') //check for the end of the word
@@ -36,15 +38,16 @@ ISR(SPI_STC_vect){                  // SPI interrupt routine {
 
 void loop (void) {
 
-if(received==true){
-  Serial.print("received new value: ");
-  Serial.println(c);
-  received=false;
-}
-  if((c==88) & (answer==true)){
-    Serial.println("starting transmission");
-    SPDR = 45,HEX;
-    answer = false;
+  if(received==true&c!=0){                // for whatever reason receives "0" after sending value
+    Serial.print("received new value: ");
+    Serial.println(c);
+    if(c==handshake){
+      SPDR = send_val, DEC;
+      delay(15);
+      Serial.print("value confirmed. sending: ");
+      Serial.println(send_val);
+    }
+    delay(15);
+    received=false;
   }
-
 }
