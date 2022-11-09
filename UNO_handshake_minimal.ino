87aab26c82ff72809455b395ee83f22b90a02db5
xaveroo <64372866+xaveroo@users.noreply.github.com>
2022-11-09 15:57:18 +0100

    Add files via upload

---
+++ UNO_handshake_minimal.ino
@@ -0,0 +1,48 @@
+#include <SPI.h>
+
+// #define DATAOUT     11  //COPI
+// #define DATAIN      12  //CIPO
+// #define SPICLOCK    13  //sck
+// #define CHIPSELECT  10  //cs
+
+// SPI peripheral
+
+char buff [50];
+byte c;
+volatile byte indx;
+volatile boolean received;
+volatile boolean answer;
+
+void setup (void) {
+   Serial.begin (115200);
+   while(!Serial){;}
+   Serial.println("Serial initiated");
+   pinMode(MISO, OUTPUT);       // have to send on master in so it set as output
+   SPCR |= _BV(SPE);            // turn on SPI in slave mode
+   indx = 0;                    // buffer empty
+   SPI.attachInterrupt(); // turn on interrupt
+}
+ISR(SPI_STC_vect){ // SPI interrupt routine { 
+   c = SPDR;        // read byte from SPI Data Register
+   received=true; 
+   answer=true;
+  //  if (indx < sizeof buff) {
+  //     buff[indx++] = c; // save data in the next index in the array buff
+  //     // if (c == '\r') //check for the end of the word
+  //  }
+}
+
+void loop (void) {
+
+if(received==true){
+  Serial.print("received new value: ");
+  Serial.println(c);
+  received=false;
+}
+  if((c==88) & (answer==true)){
+    Serial.println("starting transmission");
+    SPDR = 45,HEX;
+    answer = false;
+  }
+
+}
