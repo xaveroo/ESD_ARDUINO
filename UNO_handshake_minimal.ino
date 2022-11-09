8fc082888f97474c0eccc4099c751b2be41ac9e4
xaveroo <64372866+xaveroo@users.noreply.github.com>
2022-11-09 16:04:53 +0100

    Update UNO_handshake_minimal.ino

--- UNO_handshake_minimal.ino
+++ UNO_handshake_minimal.ino
@@ -17,15 +17,17 @@ void setup (void) {
    Serial.begin (115200);
    while(!Serial){;}
    Serial.println("Serial initiated");
-   pinMode(MISO, OUTPUT);       // have to send on master in so it set as output
-   SPCR |= _BV(SPE);            // turn on SPI in slave mode
-   indx = 0;                    // buffer empty
-   SPI.attachInterrupt(); // turn on interrupt
+   pinMode(MISO, OUTPUT);           // have to send on master in so it set as output
+   SPCR |= _BV(SPE);                // turn on SPI in slave mode
+   indx = 0;                        // buffer empty
+   SPI.attachInterrupt();           // turn on interrupt
 }
-ISR(SPI_STC_vect){ // SPI interrupt routine { 
-   c = SPDR;        // read byte from SPI Data Register
-   received=true; 
+ISR(SPI_STC_vect){                  // SPI interrupt routine { 
+   c = SPDR;                        // read byte from SPI Data Register
+   received=true;                   // raise flags
    answer=true;
+   
+  //  
   //  if (indx < sizeof buff) {
   //     buff[indx++] = c; // save data in the next index in the array buff
   //     // if (c == '\r') //check for the end of the word
